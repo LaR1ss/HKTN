337354a8019bd92b81c77c2abb0ce167e9d81b3b
LaR1ss <157617797+LaR1ss@users.noreply.github.com>
2024-04-20 14:53:48 +0600

    Delete rev.py

--- rev.py
+++
@@ -1,7 +0,0 @@
-def reverse_sentence(sentence):
-    words = sentence.split()
-    reversed_sentence = ' '.join(reversed(words))
-    return reversed_sentence
-user_input = input("Введите предложение: ")
-reversed_sentence = reverse_sentence(user_input)
-print("Обратное предложение :", reversed_sentence)
